978177c8cfbd5744cc0742c3df6bb0da79e802b8
USOD-Dev <prowodniktoka@gmail.com>
2025-10-28 21:41:22 +0200

    fix area updater perfomance issue

--- Assets/App/Gameplay/Level/AreaUpdater.cs
+++ Assets/App/Gameplay/Level/AreaUpdater.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.U2D;
 
@@ -11,15 +11,30 @@ namespace App.Gameplay.Level
         [SerializeField] private List<AreaFormPoint> points = new();
         [SerializeField] private float offsetRadius = 2.0f;
         [SerializeField] private float pointPositionThreshold = 0.3f;
-        private List<AreaFormPoint> _sortedPoints = new();
-        private List<AreaFormPoint> _insidePoints = new();
+        
+        private readonly List<Transform> _hullPoints = new();
+        private readonly List<Vector3> _sortedPositions = new();
+        private Vector3 _areaCenter;
+        private bool _isDirty = false;
 
         private void Update()
         {
-            foreach (var point in points)
+            if (!_isDirty)
             {
-                if (Vector3.Distance(point.BakedPosition, point.transform.position) > pointPositionThreshold)
+                CheckPointsMovement();
+            }
+        }
+        
+        private void CheckPointsMovement()
+        {
+            int count = points.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var point = points[i];
+                float sqrDistance = (point.BakedPosition - point.transform.position).sqrMagnitude;
+                if (sqrDistance > pointPositionThreshold * pointPositionThreshold)
                 {
+                    _isDirty = true;
                     UpdateArea();
                     return;
                 }
@@ -29,83 +44,106 @@ namespace App.Gameplay.Level
         public void AddPoint(AreaFormPoint formPoint)
         {
             points.Add(formPoint);
+            _isDirty = true;
             UpdateArea();
         }
 
         public void RemovePoint(AreaFormPoint formPoint)
         {
             points.Remove(formPoint);
+            _isDirty = true;
             UpdateArea();
         }
 
-        Vector3 GetAreaCenter()
+        private void CalculateAreaCenter()
         {
-            Vector3 sum = Vector3.zero;
-            foreach (var point in _insidePoints)
+            if (_hullPoints.Count == 0)
             {
-                sum += point.transform.position;
+                _areaCenter = Vector3.zero;
+                return;
             }
 
-            return sum / _insidePoints.Count;
+            Vector3 sum = Vector3.zero;
+            int count = _hullPoints.Count;
+            for (int i = 0; i < count; i++)
+            {
+                sum += _hullPoints[i].position;
+            }
+            _areaCenter = sum / count;
         }
 
-        private void ComputeConvexHull(List<AreaFormPoint> inputPoints)
+        private void ComputeConvexHull()
         {
-            _insidePoints.Clear();
-            if (inputPoints.Count < 3)
+            _hullPoints.Clear();
+            
+            if (points.Count < 3)
             {
-                _insidePoints.AddRange(inputPoints);
+                for (int i = 0; i < points.Count; i++)
+                {
+                    _hullPoints.Add(points[i].transform);
+                }
                 return;
             }
             
-            _sortedPoints.AddRange(inputPoints);
-            _sortedPoints.Sort((a, b) =>
+            _sortedPositions.Clear();
+            int pointCount = points.Count;
+            for (int i = 0; i < pointCount; i++)
             {
-                Vector3 posA = a.transform.position;
-                Vector3 posB = b.transform.position;
+                _sortedPositions.Add(points[i].transform.position);
+            }
+            
+            var indices = Enumerable.Range(0, pointCount).ToList();
+            indices.Sort((a, b) =>
+            {
+                Vector3 posA = _sortedPositions[a];
+                Vector3 posB = _sortedPositions[b];
 
                 if (Mathf.Approximately(posA.x, posB.x))
                     return posA.z.CompareTo(posB.z);
                 return posA.x.CompareTo(posB.x);
             });
-
-            _insidePoints.Clear();
+            
+            var hullIndices = new List<int>();
             int lowerCount = 0;
             
-            for (int i = 0; i < _sortedPoints.Count; i++)
+            for (int i = 0; i < indices.Count; i++)
             {
                 while (lowerCount >= 2 && CrossProduct(
-                           _insidePoints[lowerCount - 2].transform.position,
-                           _insidePoints[lowerCount - 1].transform.position,
-                           _sortedPoints[i].transform.position) <= 0)
+                           _sortedPositions[hullIndices[lowerCount - 2]],
+                           _sortedPositions[hullIndices[lowerCount - 1]],
+                           _sortedPositions[indices[i]]) <= 0)
                 {
-                    _insidePoints.RemoveAt(_insidePoints.Count - 1);
+                    hullIndices.RemoveAt(hullIndices.Count - 1);
                     lowerCount--;
                 }
 
-                _insidePoints.Add(_sortedPoints[i]);
+                hullIndices.Add(indices[i]);
                 lowerCount++;
             }
             
-            int upperStart = _insidePoints.Count;
-            for (int i = _sortedPoints.Count - 2; i >= 0; i--)
+            int upperStart = hullIndices.Count;
+            for (int i = indices.Count - 2; i >= 0; i--)
             {
-                while (_insidePoints.Count - upperStart >= 1 && CrossProduct(
-                           _insidePoints[^2].transform.position,
-                           _insidePoints[^1].transform.position,
-                           _sortedPoints[i].transform.position) <= 0)
+                while (hullIndices.Count - upperStart >= 1 && CrossProduct(
+                           _sortedPositions[hullIndices[^2]],
+                           _sortedPositions[hullIndices[^1]],
+                           _sortedPositions[indices[i]]) <= 0)
                 {
-                    _insidePoints.RemoveAt(_insidePoints.Count - 1);
+                    hullIndices.RemoveAt(hullIndices.Count - 1);
                 }
 
-                _insidePoints.Add(_sortedPoints[i]);
+                hullIndices.Add(indices[i]);
             }
 
-            _insidePoints.RemoveAt(_insidePoints.Count - 1);
-
-            return;
+            hullIndices.RemoveAt(hullIndices.Count - 1);
+            
+            for (int i = 0; i < hullIndices.Count; i++)
+            {
+                _hullPoints.Add(points[hullIndices[i]].transform);
+            }
         }
         
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         private float CrossProduct(Vector3 o, Vector3 a, Vector3 b)
         {
             return (a.x - o.x) * (b.z - o.z) - (a.z - o.z) * (b.x - o.x);
@@ -113,35 +151,45 @@ namespace App.Gameplay.Level
 
         public void UpdateArea()
         {
-            ComputeConvexHull(points);
-            if (_insidePoints.Count == 0)
+            ComputeConvexHull();
+            
+            if (_hullPoints.Count == 0)
             {
                 shapeController.spline.Clear();
+                _isDirty = false;
                 return;
             }
 
             shapeController.spline.Clear();
-            
-            Vector3 areaCenter = GetAreaCenter();
+            CalculateAreaCenter();
 
-            for (int i = 0; i < _insidePoints.Count; i++)
+            int count = _hullPoints.Count;
+            for (int i = 0; i < count; i++)
             {
-                Vector3 pos = _insidePoints[i].transform.position;
-                _insidePoints[i].BakedPosition = pos;
-                Vector3 dir = (pos - areaCenter).normalized;
+                var hullPoint = _hullPoints[i];
+                Vector3 pos = hullPoint.position;
+                
+                var areaFormPoint = hullPoint.GetComponent<AreaFormPoint>();
+                if (areaFormPoint != null)
+                {
+                    areaFormPoint.BakedPosition = pos;
+                }
 
-                if (dir == Vector3.zero)
+                Vector3 dir = (pos - _areaCenter).normalized;
+                if (dir.sqrMagnitude < 0.0001f)
                     dir = Vector3.up;
 
                 Vector3 offsetPos = pos + dir * offsetRadius;
                 offsetPos.y = offsetPos.z;
                 offsetPos.z = 0;
+                
                 shapeController.spline.InsertPointAt(i, offsetPos);
                 shapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
             }
 
             shapeController.BakeMesh();
             shapeController.RefreshSpriteShape();
+            _isDirty = false;
         }
 
         [ContextMenu("Update Zone")]
